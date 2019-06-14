5ab9509ffc225d125ad9a9913b30b61839d741a1
isy <ah.gm3622@gmail.com>
2019-06-14 14:57:00 +0900

    Revert "perf: fix get user query"

--- user.go
+++ user.go
@@ -10,7 +10,7 @@ type User struct {
 }
 
 func getUser(name string, address string, myNumber string) (user User, err error) {
-	row := db.QueryRow("SELECT id, votes FROM users WHERE name = ? AND address = ? AND mynumber = ?",
+	row := db.QueryRow("SELECT * FROM users WHERE name = ? AND address = ? AND mynumber = ?",
 		name, address, myNumber)
 	err = row.Scan(&user.ID, &user.Name, &user.Address, &user.MyNumber, &user.Votes)
 	return
